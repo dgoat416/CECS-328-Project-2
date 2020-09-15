ae03ce32365d315b6f15643db28c0bb19cf52c06
dgoat416 <55709445+dgoat416@users.noreply.github.com>
2020-09-14 20:25:48 -0700

    Create Main.java
    - Initial Commit
    - Provide a simple skeleton based on my current understanding of the project

---
+++ Main.java
@@ -0,0 +1,39 @@
+import java.io.File;
+
+/**
+ * Class to house the solution to Project 2
+ * @author Deron Washington II
+ * Date Started: 9/14/20 @ 8:27pm
+ * Date Finished: ? 
+ * Date Optimized: ?
+ *
+ */
+public class Main 
+{
+	
+	/**
+	 * Method to return the "score" of a recursive algorithm
+	 * @return
+	 * 				= the score of the recursive algorithm
+	 */
+	public static int countRecursions()
+	{
+		return 0;
+	}
+
+	/**
+	 * Method to write the results of countRecustions to an
+	 * output file
+	 */
+	public static void writeFile()
+	{
+		
+	}
+	
+	public static void main(String[] args) 
+	{
+		// Read input from file
+		File inFile = new File("input.txt");
+	}
+
+}
